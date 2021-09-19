3fcafc9d52463e68d71904e87452f5a2de85362a
michihiro <pomeofishbird0303@gmail.com>
2021-09-19 18:36:04 +0900

    トップページの実装

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 console.log(`listening Port ${PORT}`);
 // ejsを使えるようにするcode
 app.set("view engine", "ejs");
-
+app.disable("x-powered-by");
 // 静的ファイルの読み込み
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
