e4485ffc009afe67e22f570ad7f52522cbc31f95
michihiro <pomeofishbird0303@gmail.com>
2021-09-19 12:55:38 +0900

    初期設定の完了

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const app = express();
+const favicon = require("serve-favicon");
 const PORT = process.env.PORT;
 console.log(`listening Port ${PORT}`);
+// ejsを使えるようにするcode
+app.set("view engine", "ejs");
 
-app.get("/:id", (req, res) => {
-    res.send("Hello World" + req.params.id);
-});
+// 静的ファイルの読み込み
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// routingファイルの取り込み
+app.use("/", require("./routes/index.js"));
 
 app.listen(PORT, () => {
     console.log(`listening ${PORT}`);
